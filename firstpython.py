2a38ac8762b4adceb264a1f7eb9a5197ae55cbad
hitendrasinhzala <105972514+hitendrasinhzala@users.noreply.github.com>
2022-05-22 03:22:54 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,27 @@
 # Display the output
 print("New Python File") 
+from machinetranslation import translator
+
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    return translator.english_to_french(textToTranslate)
+    # return "Translated text to French"
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    return translator.french_to_english(textToTranslate)
+    # return "Translated text to English"
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
